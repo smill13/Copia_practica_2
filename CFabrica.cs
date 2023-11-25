b2fadf2c931f9ec509fc737e020dc8c225ffbd1f
EMMANUEL <smillriveradiaz13@gmail.com>
2023-11-25 18:35:28 -0400

    #4 Modificaciones a la Factoría/fabrica

--- CFabrica.cs
+++ CFabrica.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using Copia_practica_2.CreaciónAutos;
@@ -10,20 +12,39 @@ namespace Copia_practica_2
     class CFabrica
     {
         private static CCrearCamioneta camionetaInstance;
+        private static CCrearDeportivo deportivoIntace;
+        private static CCrearSedan sedanIntance;
+        private static CCrearMoto motoIntace;
 
-        public static IVehiculos GetVehiculos(int opciones)
+        public static IVehiculos GetVehiculos(int opcion)
         {
-            if(opciones == 1)
+
+            if (opcion == 1)
+            {
+                sedanIntance ??= new CCrearSedan();
+                return (IVehiculos)sedanIntance;
+
+            }
+            else if (opcion == 2)
+            {
+                motoIntace ??= new CCrearMoto();
+                return (IVehiculos)motoIntace;
+
+            }
+            else if (opcion == 3)
+            {
+                deportivoIntace ??= new CCrearDeportivo();
+                return (IVehiculos)deportivoIntace;
+            }
+            else if (opcion == 4)
             {
                 camionetaInstance ??= new CCrearCamioneta();
-
                 return (IVehiculos)camionetaInstance;
             }
             else
             {
                 Console.WriteLine("La opción debe estar dentro del menú");
                 return null;
-
             }
         }
     }
